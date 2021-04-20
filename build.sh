4433098a5c1e34942ed4a813ece2abfbba8e0757
许彬 <xubin@jeata.com>
2021-04-20 18:19:33 +0800

    适配

--- build.sh
+++ build.sh
@@ -4,25 +4,31 @@ set -e
 rm -rf lib
 rm -rf output
 
-./node_modules/.bin/fis3 release publish -c
+echo "生成构建"
+./node_modules/.bin/fis3 release publish -c --verbose
 rm -rf lib/node_modules
 
-# 生成 sdk
-rm -rf sdk && ./node_modules/.bin/fis3 release publish-sdk -c
+echo "去掉css变量"
+./node_modules/.bin/postcss lib/themes/default.css > lib/themes/default.css.tmp && mv -f lib/themes/default.css.tmp  lib/themes/default.css
 
-cp -r node_modules/monaco-editor/min/vs/base/browser sdk/thirds/monaco-editor/min/vs/base
+# 生成 sdk
+# echo "生成 SDK"
+# rm -rf sdk && ./node_modules/.bin/fis3 release publish-sdk -c
+
+#cp -r node_modules/monaco-editor/min/vs/base/browser sdk/thirds/monaco-editor/min/vs/base
 
 # 生成去掉变量的 css
-./node_modules/.bin/postcss sdk/sdk.css >sdk/sdk-ie11.css
-./node_modules/.bin/postcss sdk/cxd.css >sdk/cxd-ie11.css
-./node_modules/.bin/postcss sdk/dark.css >sdk/dark-ie11.css
-./node_modules/.bin/postcss sdk/antd.css >sdk/antd-ie11.css
+# ./node_modules/.bin/postcss sdk/sdk.css >sdk/sdk-ie11.css
+#./node_modules/.bin/postcss sdk/cxd.css >sdk/cxd-ie11.css
+#./node_modules/.bin/postcss sdk/dark.css >sdk/dark-ie11.css
+#./node_modules/.bin/postcss sdk/antd.css >sdk/antd-ie11.css
 
-cp ./lib/helper.css sdk/helper.css
-cp examples/static/iconfont.css sdk/
-cp examples/static/iconfont.eot sdk/
+#cp ./lib/helper.css sdk/helper.css
+#cp examples/static/iconfont.css sdk/
+#cp examples/static/iconfont.eot sdk/
 
 # 生成 .d.ts 文件
+echo "生成 .d.ts"
 ./node_modules/.bin/tsc --allowJs --declaration
 
 cd output
@@ -35,4 +41,5 @@ cd ..
 
 rm -rf output
 
+echo "生成 schemas"
 npm run build-schemas
--- publish.sh
+++ publish.sh
@@ -10,12 +10,13 @@ cp schema.json npm
 cp -rf scss npm
 cp -rf docs npm
 cp -rf examples npm
-cp -rf sdk npm
+# cp -rf sdk npm
 
 cd npm
 
 sed -i '' -e 's/\"name\": \"amis\"/\"name\": \"@jeata\/amis\"/g' ./package.json
 
+echo "发布"
 # npm publish --registry=https://nexus.jitalab.com/repository/npm/
 npm publish
 cd ..
